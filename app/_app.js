7eb0c86eed3f32a17c14895cb7ceb92f89fafb5c
Renedeo <62702704+Renedeo@users.noreply.github.com>
2023-12-20 00:01:36 +0100

    Fix <front-error>: Importation manquante de UserProvider

--- app/_app.js
+++ app/_app.js
@@ -2,6 +2,7 @@ import { MyContextProvider } from '../path/to/MyContext';
 import '../styles/globals.css';
 import { AuthProvider } from './@core/Context/AuthContext';
 import { SocketProvider } from './@core/Context/SocketContext';
+import { UserProvider } from './@core/Context/UserContext';
 
 function MyApp({ Component, pageProps }) {
     return (
